9193c4b3af866bc93c94b535eddb9bee2dfdf603
Md Sajib Khab <119875595+sajibIa@users.noreply.github.com>
2023-03-15 16:48:36 +0600

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        black: "#060606",
+        white: "#ffffff",
+        beg: "#D55DED",
+        doger: "#36DFF0",
+        yell: "#CFEC2D",
+      },
+    },
+  },
+};
